f0f4c1512d7ecc067f7b2372a5d9741c72eaeb94
Matheus Mazurechen Barros <matheusmazu@gmail.com>
2024-11-06 14:03:44 -0300

    dale

---
+++ teste.py
@@ -0,0 +1,6 @@
+while True:
+  input_usuario = input("teste: ")
+  if input_usuario:  # Verifica se o input é "verdadeiro" (qualquer valor diferente de vazio)
+    print("quero morre")
+  else:
+    print("quero me mata")
